ae878e3082780856a99834259c5006a9f9236261
William Bu <williambu1027@gmail.com>
2024-10-05 10:08:49 -0400

    setup basic gpt wrapper with api get request

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from openai import OpenAI
+
+
+client = OpenAI(
+    api_key="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"
+)
+def chatgpt_message(prompt):
+    chat_completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "assistant", "content": "You are a travel vacation assistant"},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    return chat_completion.choices[0].message.content.strip()
+
+
+example_gpt = chatgpt_message("give me a one sentence overview of paris")
+print(example_gpt)
+
 
 app = FastAPI()
 
@@ -20,7 +43,7 @@ app.add_middleware(
 
 @app.get("/")
 async def get_data():
-    data = {'name': 'WILLIAM BU TESTING',
+    data = {'name': example_gpt,
             'age': 20}
     return data
 
